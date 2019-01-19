e4a1031829eb2c560318530b3b1ff70affc0fee3
JSchielka <jschielka@gmail.com>
2019-01-19 16:51:44 -0500

    Final

--- Belly_Button_Biodiversity/static/js/app.js
+++ Belly_Button_Biodiversity/static/js/app.js
@@ -3,27 +3,75 @@ function buildMetadata(sample) {
   // @TODO: Complete the following function that builds the metadata panel
 
   // Use `d3.json` to fetch the metadata for a sample
+  var url = `metadata/${sample}`;
+  d3.json(url).then(function(sample) {
+
     // Use d3 to select the panel with id of `#sample-metadata`
-
+    d3.select("#sample-metadata")
     // Use `.html("") to clear any existing metadata
-
+    .html("")
     // Use `Object.entries` to add each key and value pair to the panel
     // Hint: Inside the loop, you will need to use d3 to append new
     // tags for each key-value in the metadata.
-
+    var newData = Object.entries(sample).forEach(([key, value]) => {
+    console.log(`${key}: ${value}`);
+    });
+    d3.select("#sample-metadata")
+      .append("panel-body")
+      .text(newData);
+  });
+}
     // BONUS: Build the Gauge Chart
     // buildGauge(data.WFREQ);
-}
+
 
 function buildCharts(sample) {
 
   // @TODO: Use `d3.json` to fetch the sample data for the plots
+  var url = `/samples/${sample}`;
+  d3.json(url).then(function(data) {
 
+    console.log(data);
+  
     // @TODO: Build a Bubble Chart using the sample data
+    var trace = {
+      x: data.otu_ids,
+      y: data.sample_values,
+      type: 'scatter',
+      mode: 'markers',
+      marker: {
+        color: data.otu_ids,
+        opacity: 0.5,
+        size: data.sample_values,
+        text: data.otu_labels
+      }
+    };
+
+    console.log(trace);
+    
+    var layout = {
+      title: 'Bubble Chart',
+      showlegend: false,
+    };
+    
+    Plotly.plot('bubble', [trace], layout);
 
     // @TODO: Build a Pie Chart
     // HINT: You will need to use slice() to grab the top 10 sample_values,
     // otu_ids, and labels (10 each).
+    var data = [{
+      values: data.sample_values.slice(0 ,10),
+      labels: data.otu_ids.slice(0 ,10),
+      hovertext: data.otu_labels.slice(0 ,10),
+      type: 'pie',
+    }];
+    
+    var layout = {
+      showlegend: true,
+    };
+    
+    Plotly.newPlot('pie', data, layout); 
+  });
 }
 
 function init() {
